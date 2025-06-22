38957c62f66489694a7fc4ab327bb5bcc76fea67
rajeeb0423 <rajeeb.sharma@nbi.ku.dk>
2025-06-22 22:51:24 +0200

    few changes

--- index.py
+++ index.py
@@ -5,7 +5,7 @@ def main():
     ascii_image = st.Page('ascii_image.py', title='Image to ASCII generator')
     ascii_gif = st.Page('ascii_gif.py', title='GIF to ASCII generator')
 
-    pg = st.navigation([ascii_image, ascii_gif], expanded=True)#, position='hidden')
+    pg = st.navigation([ascii_image, ascii_gif])#, position='hidden')
 
     st.set_page_config(page_title= 'Image to ASCII geneartor', layout='wide', initial_sidebar_state='expanded')
     pg.run()
